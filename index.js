ce090edef42a85418a012a17bd7dc81e7f61cad9
cse-f <verify.moneymind@gmail.com>
2024-11-15 00:48:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
-1)JDBC-SELECT---->         curl https://cse-f.github.io/wt/jdbc/1.js
+
 1)JQUERY-EVENTS(mouseUP,Down,enter,leave,focus)---->        curl https://cse-f.github.io/wt/jq/1.js
 1)JQUERY-ACTIONS(show,hide,toggle)---->        curl https://cse-f.github.io/wt/jq/2.js
 1)JQUERY-SLIDEMETHODS---->       curl https://cse-f.github.io/wt/jq/4.js
 2)JQUERY-FADEMETHODS---->       curl https://cse-f.github.io/wt/jq/3.js
+
+1)JDBC-SELECT---->         curl https://cse-f.github.io/wt/jdbc/1.js
+1)JDBC-INSERT,Update,Delete---->         curl https://cse-f.github.io/wt/jdbc/2.js
+1)JDBC-preparedStatement,insert---->         curl https://cse-f.github.io/wt/jdbc/3.js
+1)JDBC-procedure---->         curl https://cse-f.github.io/wt/jdbc/4.js
+1)JDBC-databaseMETADATA---->         curl https://cse-f.github.io/wt/jdbc/5.js
+1)JDBC-resultSET METADATA---->         curl https://cse-f.github.io/wt/jdbc/6.js
 
