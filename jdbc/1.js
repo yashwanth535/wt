c9ee82bd8752315b104b66595cda8b08344833ea
cse-f <verify.moneymind@gmail.com>
2024-11-14 23:33:13 +0530

    Create 1.js

---
+++ jdbc/1.js
@@ -0,0 +1,20 @@
+//1.select in jdbc
+
+package jdbc;
+import java.sql.*;
+class select{
+	public static void main(String args[]) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","yashwanth","cvrbw");
+			Statement stmt=conn.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from student");
+			while(rs.next()) {
+				System.out.println(rs.getString(1)+" "+rs.getInt(2)+" "+rs.getString(3));	
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
