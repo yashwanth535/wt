f7266702114d3c618b1876970c620435dd31d4ae
cse-f <verify.moneymind@gmail.com>
2024-11-14 22:53:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+1)JQUERY-EVENTS(mouseUP,Down,enter,leave,focus)---->curl https://cse-f.github.io/wt/jq/1.js
+1)JQUERY-ACTIONS(show,hide,toggle)---->curl https://cse-f.github.io/wt/jq/2.js
 1)JQUERY-SLIDEMETHODS---->curl https://cse-f.github.io/wt/jq/4.js
 2)JQUERY-FADEMETHODS---->curl https://cse-f.github.io/wt/jq/3.js
+
