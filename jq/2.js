d8fe313c684a9ce404a52047edb2f5e2b1a5bf25
cse-f <verify.moneymind@gmail.com>
2024-11-14 18:18:43 +0530

    Update 2.js

--- jq/2.js
+++ jq/2.js
@@ -1,1 +1,35 @@
-second
+2.jquery effects--show,hide,toggle
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+</head>
+<body>
+  <button id="hide-btn">Hide</button>
+  <button id="show-btn">show</button>
+  <button id="toggle-btn">toggle</button>
+  <div id="content" style="margin-top: 20px;">this is the content</div>
+
+</body>
+<script>
+ $(document).ready(function(){
+  $("#hide-btn").click(function(){
+    $("#content").hide();
+  });
+
+  $('#show-btn').click(function(){
+    $('#content').show();
+  });
+
+  $('#toggle-btn').click(function(){
+    $('#content').toggle();
+  })
+ });
+
+</script>
+</html>
