9620a28d39b33c90a720ef8c7e71f620087c409a
cse-f <verify.moneymind@gmail.com>
2024-11-18 20:22:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 
-JQUERY-EVENTS(mouseUP,Down,enter,leave,focus)---->        curl https://cse-f.github.io/wt/jq/1.js
-JQUERY-ACTIONS(show,hide,toggle)---->        curl https://cse-f.github.io/wt/jq/2.js
-JQUERY-SLIDEMETHODS---->       curl https://cse-f.github.io/wt/jq/4.js
-JQUERY-FADEMETHODS---->       curl https://cse-f.github.io/wt/jq/3.js
+JQUERY-EVENTS(mouseUP,Down,enter,leave,focus)---->        curl -k https://cse-f.github.io/wt/jq/1.js
+JQUERY-ACTIONS(show,hide,t)---->       curl -k https://cse-f.github.io/wt/jq/2.js
+JQUERY-SLIDEMETHODS------------>       curl -k https://cse-f.github.io/wt/jq/4.js
+JQUERY-FADEMETHODS------------->       curl -k https://cse-f.github.io/wt/jq/3.js
 
-JDBC-SELECT---->         curl https://cse-f.github.io/wt/jdbc/1.js
-JDBC-INSERT,Update,Delete---->         curl https://cse-f.github.io/wt/jdbc/2.js
-JDBC-preparedStatement,insert---->         curl https://cse-f.github.io/wt/jdbc/3.js
-JDBC-procedure---->         curl https://cse-f.github.io/wt/jdbc/4.js
-JDBC-databaseMETADATA---->         curl https://cse-f.github.io/wt/jdbc/5.js
-JDBC-resultSET METADATA---->         curl https://cse-f.github.io/wt/jdbc/6.js
+JDBC-SELECT--------------------->         curl -k https://cse-f.github.io/wt/jdbc/1.js
+JDBC-INSERT,Update,Delete------->         curl -k https://cse-f.github.io/wt/jdbc/2.js
+JDBC-preparedStatement,insert--->         curl -k https://cse-f.github.io/wt/jdbc/3.js
+JDBC-procedure------------------>         curl -k https://cse-f.github.io/wt/jdbc/4.js
+JDBC-databaseMETADATA----------->         curl -k https://cse-f.github.io/wt/jdbc/5.js
+JDBC-resultSET METADATA--------->         curl -k https://cse-f.github.io/wt/jdbc/6.js
 
